9bf7e41e9b2b9ca8bc7eb872673ed11ed18683a3
Eoin Irwin <C13439802@mydit.ie>
2015-12-04 23:26:40 +0000

    Register page with a navbar.

--- views/register.php
+++ views/register.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: Eoin's
  * Date: 04/12/2015
- * Time: 15:47
+ * Time: 22:51
  */
 ?>
 <!DOCTYPE html>
@@ -14,7 +14,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PC Builder</title>
+    <title>Bootstrap Registration Form Template</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
@@ -31,7 +31,7 @@
     <![endif]-->
 
     <!-- Favicon and touch icons -->
-    <link rel="shortcut icon" href="styles/assets/ico/favicon.png">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144"
           href="styles/assets/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114"
@@ -49,7 +49,7 @@
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
             </button>
-            <a class="navbar-brand" href="register.php">Register</a>
+            <a class="navbar-brand" href="view_login.php">Login</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="top-navbar-1">
@@ -65,42 +65,50 @@
     <div class="inner-bg">
         <div class="container">
             <div class="row">
-                <div class="col-sm-8 col-sm-offset-2 text">
-                    <h1><strong>PC Builder</strong> Login</h1>
+                <div class="col-sm-7 text">
+                    <h1><strong>Pc Builder</strong> Registration</h1>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 col-sm-offset-3 form-box">
+                <div class="col-sm-5 form-box">
                     <div class="form-top">
                         <div class="form-top-left">
-                            <h3>Login to our site</h3>
+                            <h3>Sign up now</h3>
 
-                            <p>Enter your username and password to log on:</p>
+                            <p>Fill in the form below to get instant access:</p>
                         </div>
                         <div class="form-top-right">
-                            <i class="fa fa-key"></i>
+                            <i class="fa fa-pencil"></i>
                         </div>
                     </div>
                     <div class="form-bottom">
-                        <form role="form" action="" method="post" class="login-form">
+                        <form role="form" action="" method="post" class="registration-form">
                             <div class="form-group">
-                                <label class="sr-only" for="form-username">Username</label>
-                                <input type="text" name="form-username" placeholder="Username..."
-                                       class="form-username form-control" id="form-username">
+                                <label class="sr-only" for="form-first-name">First name</label>
+                                <input type="text" name="form-first-name" placeholder="First name..."
+                                       class="form-first-name form-control" id="form-first-name">
                             </div>
                             <div class="form-group">
-                                <label class="sr-only" for="form-password">Password</label>
-                                <input type="password" name="form-password" placeholder="Password..."
-                                       class="form-password form-control" id="form-password">
+                                <label class="sr-only" for="form-last-name">Last name</label>
+                                <input type="text" name="form-last-name" placeholder="Last name..."
+                                       class="form-last-name form-control" id="form-last-name">
                             </div>
-                            <button type="submit" class="btn">Sign in!</button>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-email">Email</label>
+                                <input type="text" name="form-email" placeholder="Email..."
+                                       class="form-email form-control" id="form-email">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-about-yourself">About yourself</label>
+			                        	<textarea name="form-about-yourself" placeholder="About yourself..."
+                                                  class="form-about-yourself form-control"
+                                                  id="form-about-yourself"></textarea>
+                            </div>
+                            <button type="submit" class="btn">Sign me up!</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 </div>
 
@@ -109,6 +117,7 @@
 <script src="styles/assets/js/jquery-1.11.1.min.js"></script>
 <script src="styles/assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="styles/assets/js/jquery.backstretch.min.js"></script>
+<script src="styles/assets/js/retina-1.1.0.min.js"></script>
 <script src="styles/assets/js/scripts.js"></script>
 
 <!--[if lt IE 10]>
