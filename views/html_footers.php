baf0dea9d6481c1abdece467d4efdd5459faedef
barrypearseburke <barry.burke3@mydit.ie>
2015-12-08 20:11:57 +0000

    Headers and footers in seperate file

---
+++ views/html_footers.php
@@ -0,0 +1,15 @@
+
+
+<!-- Javascript -->
+<script src="styles/assets/js/jquery-1.11.1.min.js"></script>
+<script src="styles/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="styles/assets/js/jquery.backstretch.min.js"></script>
+<script src="styles/assets/js/scripts.js"></script>
+
+<!--[if lt IE 10]>
+<script src="styles/assets/js/placeholder.js"></script>
+<![endif]-->
+
+</body>
+
+</html>
--- views/register_template.php
+++ views/register_template.php
@@ -1,47 +1,7 @@
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Eoin's
- * Date: 04/12/2015
- * Time: 22:51
- */
+include_once("html_headers.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Registration Form Template</title>
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-    <link rel="stylesheet" href="styles/assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/assets/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="styles/assets/css/form-elements.css">
-    <link rel="stylesheet" href="styles/assets/css/style.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Favicon and touch icons -->
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144"
-          href="styles/assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114"
-          href="styles/assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="styles/assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="styles/assets/ico/apple-touch-icon-57-precomposed.png">
-
-</head>
-
-<body>
 
 <!-- Top menu -->
 <nav class="navbar navbar-inverse navbar-no-bg" role="navigation">
@@ -114,17 +74,6 @@
 </div>
 
 
-<!-- Javascript -->
-<script src="styles/assets/js/jquery-1.11.1.min.js"></script>
-<script src="styles/assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="styles/assets/js/jquery.backstretch.min.js"></script>
-<script src="styles/assets/js/retina-1.1.0.min.js"></script>
-<script src="styles/assets/js/scripts.js"></script>
-
-<!--[if lt IE 10]>
-<script src="styles/assets/js/placeholder.js"></script>
-<![endif]-->
-
-</body>
-
-</html>
+<?php
+include_once("html_footers.php");
+?>
