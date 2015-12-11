9b6233fb1a3a2003faa262d625d19caeb341400d
Eoin Irwin <C13439802@mydit.ie>
2015-12-11 20:42:41 +0000

    Home template

--- views/home_template.php
+++ views/home_template.php
@@ -5,18 +5,62 @@
  * Date: 10/12/2015
  * Time: 23:24
  */
-include("html_headers.php");
+include_once("html_nav_headers.php");
 
 ?>
 
-<p>You have been logged on:</p>
 
 
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eoin's
+ * Date: 04/12/2015
+ * Time: 15:47
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PC Builder</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+    <link rel="stylesheet" href="styles/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/assets/css/form-elements.css">
+    <link rel="stylesheet" href="styles/assets/css/style.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Favicon and touch icons -->
+    <link rel="shortcut icon" href="../styles/assets/ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144"
+          href="../styles/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114"
+          href="../styles/assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72"
+          href="../styles/assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../styles/assets/ico/apple-touch-icon-57-precomposed.png">
+
+</head>
+
+<body>
 
 
 
 
 <?php
 
-include("html_footers.php");
+include_once("html_footers.php");
 ?>
