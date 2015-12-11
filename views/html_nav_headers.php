16e7ba8c2bbbb7f76a47955111501470888fbd00
Eoin Irwin <C13439802@mydit.ie>
2015-12-11 20:42:07 +0000

    HTML Navbar header

---
+++ views/html_nav_headers.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eoin's
+ * Date: 11/12/2015
+ * Time: 20:26
+ */
+?>
+<!-- Top menu -->
+<nav class="navbar navbar-inverse navbar-no-bg" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+            </button>
+            <a class="navbar-brand" href="index.php">Hello </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="top-navbar-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="parts.php">Parts</a></li>
+                <li><a href="builds.php">Builds</a></li>
+                <li><a href="analytics.php">Analytics</a></li>
+                <li><a href="logout.php">Logout </a></li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
