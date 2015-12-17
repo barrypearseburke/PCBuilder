e06c12ae93a4333222b383ca409e71c93e04a1aa
Eoin Irwin <C13439802@mydit.ie>
2015-12-17 22:52:16 +0000

    Working html headers with random bg images

--- views/html_headers.php
+++ views/html_headers.php
@@ -8,8 +8,8 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,3 +43,12 @@
 </head>
 
 <body>
+
+<script type="text/javascript">
+    var bgcount = 7;
+    function changebg() {
+        var num = Math.ceil(Math.random() * bgcount);
+        document.body.background = 'styles/assets/img/backgrounds/' + num + '.png';
+    }
+    changebg();
+</script>
