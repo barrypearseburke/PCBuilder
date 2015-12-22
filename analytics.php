0b3ea33afb3cd79d92de1e94f660bc82c3b9ed4f
Eoin Irwin <C13439802@mydit.ie>
2015-12-22 01:19:01 +0000

    Analytics being added

---
+++ analytics.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barry and Eoin
+ * Date: 10/12/2015
+ * Time: 23:23
+ */
+
+include_once("models/model.php");
+include("controllers/controller.php");
+include("views/view.php");
+include "conf/config.inc.php";
+session_start();
+$action = "";
+if (!empty ($_REQUEST ['action']))
+    $action = $_REQUEST ['action'];
+
+$model = new Model ();
+$controller = new Controller ($model, $action);
+$controller->login_success();
+$view = new View ($controller, $model);
+$page = "analytics_template.php";
+
+// note: there is no need to echo the output (echos are within the template)
+$view->getHTMLOutput($page);
+?>
