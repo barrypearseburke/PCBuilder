e5b34cf1b60b943a51f9627fc01cafb2d015d049
Eoin Irwin <C13439802@mydit.ie>
2016-01-09 18:48:50 +0000

    Better changes

--- views/footer.php
+++ views/footer.php
@@ -9,7 +9,7 @@
 
 <div class="navbar navbar-inverse navbar-fixed-bottom" id ="footer">
     <div class="navbar-text float right" id ="footer-text">
-        <p class="navbar-text">Barry Burke | C13427078 | Eoin Irwin | C13439802 | Group H</p>
+        <p class="navbar-text">Eoin Irwin | C13439802 | Barry Burke | C13427078 | | Group H</p>
     </div>
 </div>
 </div>
